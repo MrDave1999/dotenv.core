74f5816fa4dbd61e268ca2ee08a9cd8d61059523
MrDave1999 <daveseva2010@hotmail.es>
2021-12-16 11:40:46 -0500

    Create ExceptionMessages.cs

---
+++ src/Constants/ExceptionMessages.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotEnv.Core
+{
+    /// <summary>
+    /// Represents the messages of an exception.
+    /// </summary>
+    public class ExceptionMessages
+    {
+        public const string VariableNotFoundMessage = "The environment variable was not found in the current process.";
+        public const string InputIsEmptyOrWhitespaceMessage = "The data source (probably the env file) is empty or consists only in whitespace.";
+        public const string KeyIsAnEmptyStringMessage = "The parser found an empty key (\"\").";
+        public const string LineHasNoKeyValuePairMessage = "The parser found a line that has no key-value pair format.";
+    }
+}
