8179c43604975fb142a8ca6d092ee0f18d277463
MrDave1999 <daveseva2010@hotmail.es>
2021-12-27 14:12:59 -0500

    Create ConcatKeysOptions.cs

---
+++ src/Constants/ConcatKeysOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotEnv.Core
+{
+    /// <summary>
+    /// Specifies the options for concatenation of duplicate keys such as whether to concatenate at the start or end of the value.
+    /// </summary>
+    public enum ConcatKeysOptions
+    {
+        /// <summary>
+        /// This option is only used as the default value of the <see cref="EnvParserOptions.ConcatDuplicateKeys" /> property.
+        /// </summary>
+        None,
+        /// <summary>
+        /// This option will tell the parser to concatenate at the beginning of the value of a duplicate key.
+        /// </summary>
+        Start,
+        /// <summary>
+        /// This option will tell the parser to concatenate at the end of the value of a duplicate key.
+        /// </summary>
+        End
+    }
+}
