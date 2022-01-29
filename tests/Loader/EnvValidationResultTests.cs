f7c2304d9b12046b9a7b19436dde8cc689dcf4fb
MrDave1999 <daveseva2010@hotmail.es>
2022-01-29 14:38:51 -0500

    Added the unit tests (issue #13)

---
+++ tests/Loader/EnvValidationResultTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static DotEnv.Core.ExceptionMessages;
+using static System.IO.Path;
+
+namespace DotEnv.Core.Tests.Loader
+{
+    [TestClass]
+    public class EnvValidationResultTests
+    {
+        [TestMethod]
+        public void Load_WhenErrorsAreFound_ShouldReadTheErrors()
+        {
+            string msg;
+            EnvValidationResult result;
+            string basePath = $"Loader{DirectorySeparatorChar}env_files{DirectorySeparatorChar}";
+            new EnvLoader()
+                .SetBasePath(basePath)
+                .IgnoreParserExceptions()
+                .AddEnvFile(".env.validation.result1")
+                .AddEnvFile(".env.validation.result2")
+                .AddEnvFile(".env.validation.result3")
+                .AddEnvFile(".env.validation.result4")
+                .AddEnvFiles(".env.not.found3", ".env.not.found4", ".env.not.found5", ".env.not.found6")
+                .Load(out result);
+
+            msg = result.ErrorMessages;
+            Assert.AreEqual(result.HasError(), true);
+
+            StringAssert.Contains(msg, $"{LineHasNoKeyValuePairMessage} (Actual Value: This is an error, Line: 1, FileName: {basePath}.env.validation.result1)");
+            StringAssert.Contains(msg, $"{KeyIsAnEmptyStringMessage} (Line: 2, FileName: {basePath}.env.validation.result1)");
+            StringAssert.Contains(msg, $"{VariableNotFoundMessage} (Actual Value: VARIABLE_NOT_FOUND, Line: 3, FileName: {basePath}.env.validation.result1)");
+            StringAssert.Contains(msg, $"{VariableNotFoundMessage} (Actual Value: VARIABLE_NOT_FOUND_2, Line: 3, FileName: {basePath}.env.validation.result1)");
+            StringAssert.Contains(msg, $"{VariableIsAnEmptyStringMessage} (Line: 5, FileName: {basePath}.env.validation.result1)");
+            StringAssert.Contains(msg, $"{VariableIsAnEmptyStringMessage} (Line: 5, FileName: {basePath}.env.validation.result1)");
+
+            StringAssert.Contains(msg, $"{InputIsEmptyOrWhitespaceMessage} (FileName: {basePath}.env.validation.result2)");
+
+            StringAssert.Contains(msg, $"{LineHasNoKeyValuePairMessage} (Actual Value: This is a line, Line: 1, FileName: {basePath}.env.validation.result3)");
+            StringAssert.Contains(msg, $"{KeyIsAnEmptyStringMessage} (Line: 2, FileName: {basePath}.env.validation.result3)");
+            StringAssert.Contains(msg, $"{VariableNotFoundMessage} (Actual Value: VARIABLE_NOT_FOUND, Line: 3, FileName: {basePath}.env.validation.result3)");
+
+            StringAssert.Contains(msg, $"{LineHasNoKeyValuePairMessage} (Actual Value: This is a message, Line: 1, FileName: {basePath}.env.validation.result4)");
+            StringAssert.Contains(msg, $"{KeyIsAnEmptyStringMessage} (Line: 2, FileName: {basePath}.env.validation.result4)");
+
+            StringAssert.Contains(msg, $"{FileNotFoundMessage} (FileName: {basePath}.env.not.found3)");
+            StringAssert.Contains(msg, $"{FileNotFoundMessage} (FileName: {basePath}.env.not.found4)");
+            StringAssert.Contains(msg, $"{FileNotFoundMessage} (FileName: {basePath}.env.not.found5)");
+            StringAssert.Contains(msg, $"{FileNotFoundMessage} (FileName: {basePath}.env.not.found6)");
+        }
+    }
+}
---
+++ tests/Parser/EnvValidationResultTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static DotEnv.Core.ExceptionMessages;
+
+namespace DotEnv.Core.Tests.Parser
+{
+    [TestClass]
+    public class EnvValidationResultTests
+    {
+        [TestMethod]
+        public void Parse_WhenErrorsAreFound_ShouldReadTheErrors()
+        {
+            var parser = new EnvParser().IgnoreParserExceptions();
+            string env, msg;
+            EnvValidationResult result;
+
+            env = @"This is an error
+                =VAL1
+                ERRORS_ARE_FOUND_1 = ${VARIABLE_NOT_FOUND} World! ${VARIABLE_NOT_FOUND_2}
+                ERRORS_ARE_FOUND_3 = VAL
+                ERRORS_ARE_FOUND_2 = ${} World! Hello ${   }
+            ";
+            parser.Parse(env, out _);
+
+            env = null;
+            parser.Parse(env, out _);
+
+            env = @"This is a line
+                =VAL2
+                ERRORS_ARE_FOUND_4 = ${VARIABLE_NOT_FOUND}!!
+            ";
+            parser.Parse(env, out _);
+
+            env = @"This is a message
+                =VAL3
+            ";
+            parser.Parse(env, out result);
+
+            msg = result.ErrorMessages;
+            Assert.AreEqual(result.HasError(), true);
+
+            StringAssert.Contains(msg, $"{LineHasNoKeyValuePairMessage} (Actual Value: This is an error, Line: 1)");
+            StringAssert.Contains(msg, $"{KeyIsAnEmptyStringMessage} (Line: 2)");
+            StringAssert.Contains(msg, $"{VariableNotFoundMessage} (Actual Value: VARIABLE_NOT_FOUND, Line: 3)");
+            StringAssert.Contains(msg, $"{VariableNotFoundMessage} (Actual Value: VARIABLE_NOT_FOUND_2, Line: 3)");
+            StringAssert.Contains(msg, $"{VariableIsAnEmptyStringMessage} (Line: 5)");
+            StringAssert.Contains(msg, $"{VariableIsAnEmptyStringMessage} (Line: 5)");
+
+            StringAssert.Contains(msg, InputIsEmptyOrWhitespaceMessage);
+
+            StringAssert.Contains(msg, $"{LineHasNoKeyValuePairMessage} (Actual Value: This is a line, Line: 1)");
+            StringAssert.Contains(msg, $"{KeyIsAnEmptyStringMessage} (Line: 2)");
+            StringAssert.Contains(msg, $"{VariableNotFoundMessage} (Actual Value: VARIABLE_NOT_FOUND, Line: 3)");
+
+            StringAssert.Contains(msg, $"{LineHasNoKeyValuePairMessage} (Actual Value: This is a message, Line: 1)");
+            StringAssert.Contains(msg, $"{KeyIsAnEmptyStringMessage} (Line: 2)");
+        }
+    }
+}
