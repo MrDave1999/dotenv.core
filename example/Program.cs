2ea0a030be0a3f4ca28d0acdb3d213aeb2950b41
MrDave1999 <daveseva2010@hotmail.es>
2022-02-07 10:57:41 -0500

    Added new code sample to the example project

--- example/Program.cs
+++ example/Program.cs
@@ -18,53 +18,94 @@ namespace DotEnv.Core.Example
 		{
 			var reader = new EnvReader();
 
+			Console.WriteLine("---- EXAMPLE (1):");
 			new EnvLoader()
 				.EnableFileNotFoundException()
 				.Load();
 			Console.WriteLine($"MYSQL_HOST={reader["MYSQL_HOST"]}");
 			Console.WriteLine($"MYSQL_DB={reader["MYSQL_DB"]}");
+			Console.WriteLine("\n\n\n");
 
+			Console.WriteLine("---- EXAMPLE (2):");
 			new EnvLoader()
 				.SetBasePath("files")
 				.Load();
 			Console.WriteLine($"MARIADB_HOST={reader["MARIADB_HOST"]}");
 			Console.WriteLine($"MARIADB_DB={reader["MARIADB_DB"]}");
+			Console.WriteLine("\n\n\n");
 
+			Console.WriteLine("---- EXAMPLE (3):");
 			new EnvLoader()
 				.AddEnvFile("./files/.env.example")
 				.Load();
 			Console.WriteLine($"MONGODB_HOST={reader["MONGODB_HOST"]}");
 			Console.WriteLine($"MONGODB_DB={reader["MONGODB_DB"]}");
+			Console.WriteLine("\n\n\n");
 
+			Console.WriteLine("---- EXAMPLE (4):");
 			new EnvLoader()
 				.AddEnvFiles("files/sqlite")
 				.EnableFileNotFoundException()
 				.Load();
 			Console.WriteLine($"SQLITE_HOST={reader["SQLITE_HOST"]}");
 			Console.WriteLine($"SQLITE_DB={reader["SQLITE_DB"]}");
+			Console.WriteLine("\n\n\n");
 
+			Console.WriteLine("---- EXAMPLE (5):");
 			new EnvLoader()
 				.SetDefaultEnvFileName(".env.local")
 				.SetBasePath("./files/pgsql")
 				.Load();
 			Console.WriteLine($"PGSQL_HOST={reader["PGSQL_HOST"]}");
 			Console.WriteLine($"PGSQL_DB={reader["PGSQL_DB"]}");
+			Console.WriteLine("\n\n\n");
 
+			Console.WriteLine("---- EXAMPLE (6):");
 			new EnvLoader(new CustomEnvParser())
 				.DisableParserException()
 				.SetDefaultEnvFileName("./files/.env.local")
 				.Load(out var result);
 
-			Console.WriteLine($"API_KEY LENGTH={reader["API_KEY"]}");
+			Console.WriteLine($"API_KEY LENGTH={reader["API_KEY"].Length}");
 			Console.WriteLine(result.ErrorMessages);
+			Console.WriteLine("\n\n\n");
 
+			Console.WriteLine("---- EXAMPLE (7):");
 			var dict = new EnvLoader()
 						.AvoidModifyEnvironment()
 						.AddEnvFiles("./", "./files/", "files/sqlite", "./files/pgsql/.env.local")
 						.Load();
-			Console.WriteLine("\n\n\nDictionary:");
+			Console.WriteLine("Dictionary:");
 			foreach (var pair in dict)
 				Console.WriteLine($"{pair.Key}, {pair.Value}");
+			Console.WriteLine("\n\n\n");
+
+			Console.WriteLine("---- EXAMPLE (8):");
+			new EnvLoader()
+				.SetBasePath("./files/environment")
+				.AllowConcatDuplicateKeys()
+				.LoadEnv(out result);
+			if (result.HasError())
+				Console.WriteLine(result.ErrorMessages);
+			else
+				Console.WriteLine($"CONNECTION_STRING_MSSQL={reader["CONNECTION_STRING_MSSQL"]}");
+			Console.WriteLine("\n\n\n");
+
+			Console.WriteLine("---- EXAMPLE (9):");
+			dict = new EnvLoader()
+					.AllowConcatDuplicateKeys()
+					.SetBasePath("./files/environment")
+					.AvoidModifyEnvironment()
+					.LoadEnv(out result);
+
+			if(result.HasError())
+				Console.WriteLine(result.ErrorMessages);
+			else
+            {
+				foreach(var pair in dict)
+					Console.WriteLine($"{pair.Key}, {pair.Value}");
+			}
+			Console.WriteLine("\n\n\n");
 		}
 	}
 }
