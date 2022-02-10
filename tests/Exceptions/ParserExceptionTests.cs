8b27d13e5f3a2c4d13ccca550c94f46cd605cb74
MrDave1999 <daveseva2010@hotmail.es>
2022-02-10 13:57:44 -0500

    Update ParserExceptionTests.cs

--- tests/Exceptions/ParserExceptionTests.cs
+++ tests/Exceptions/ParserExceptionTests.cs
@@ -10,44 +10,32 @@ namespace DotEnv.Core.Tests.Exceptions
         public void Message_WhenPassingActualValueAndCurrentLine_ShouldReturnMessageWithActualValueAndCurrentLine()
         {
             var parser = new ParserException("This is a Message.", actualValue: 1, currentLine: 1);
-            string expected = "This is a Message. (Actual Value: 1, Line: 1)";
 
-            string actual = parser.Message;
-
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected: "This is a Message. (Actual Value: 1, Line: 1)", actual: parser.Message);
         }
 
         [TestMethod]
         public void Message_WhenPassingActualValue_ShouldReturnMessageWithActualValue()
         {
             var parser = new ParserException("This is a Message.", actualValue: 1);
-            string expected = "This is a Message. (Actual Value: 1)";
 
-            string actual = parser.Message;
-
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected: "This is a Message. (Actual Value: 1)", actual: parser.Message);
         }
 
         [TestMethod]
         public void Message_WhenPassingCurrentLine_ShouldReturnMessageWithCurrentLine()
         {
             var parser = new ParserException("This is a Message.", currentLine: 1);
-            string expected = "This is a Message. (Line: 1)";
 
-            string actual = parser.Message;
-
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected: "This is a Message. (Line: 1)", actual: parser.Message);
         }
 
         [TestMethod]
         public void Message_WhenNotPassingActualValueAndCurrentLine_ShouldReturnMessage()
         {
             var parser = new ParserException("This is a Message.");
-            string expected = "This is a Message.";
 
-            string actual = parser.Message;
-
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(expected: "This is a Message.", actual: parser.Message);
         }
     }
 }
