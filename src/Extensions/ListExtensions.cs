3b6ba151cb07c77021cadfa8f21dab26a0b2e9d1
MrDave1999 <daveseva2010@hotmail.es>
2022-02-11 11:48:53 -0500

    The 'IsEmpty' method was added and the 'NotExists' method was removed

--- src/Extensions/ListExtensions.cs
+++ src/Extensions/ListExtensions.cs
@@ -7,12 +7,10 @@ namespace DotEnv.Core
     internal static class ListExtensions
     {
         /// <summary>
-        /// Check if the .env file exists in any directory.
+        /// Checks if the collection is empty.
         /// </summary>
-        /// <param name="envFiles">The collection of .env files.</param>
-        /// <param name="envFileName">The name of the .env file.</param>
-        /// <returns>true if the .env file does not exist, otherwise false.</returns>
-        public static bool NotExists(this List<EnvFile> envFiles, string envFileName)
-            => !envFiles.Find(envFile => envFile.Path.IndexOf(envFileName) != -1).Exists;
+        /// <returns>true if the collecion is empty, or false</returns>
+        public static bool IsEmpty<T>(this List<T> list)
+            => list.Count == 0;
     }
 }
