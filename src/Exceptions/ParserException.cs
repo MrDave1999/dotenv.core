cc8424837bfd832bf25ccc62c6011c201a1c792b
MrDave1999 <daveseva2010@hotmail.es>
2022-02-26 14:45:06 -0500

    The actualValue and currentLine parameters have been removed from the constructor of the 'ParserException' class

--- src/Exceptions/ParserException.cs
+++ src/Exceptions/ParserException.cs
@@ -9,68 +9,20 @@ namespace DotEnv.Core
     /// </summary>
     public class ParserException : Exception
     {
-        private readonly object _actualValue;
-        private readonly int? _currentLine;
-
         /// <summary>
-        /// Allows access to the actual value that causes the exception.
+        /// /// Initializes a new instance of the <see cref="ParserException" /> class.
         /// </summary>
-        public object ActualValue => _actualValue;
-
-        /// <summary>
-        /// Allows access to the current line that causes the exception.
-        /// </summary>
-        public int? CurrentLine => _currentLine;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ParserException" /> class with the a specified error message, the actual value and the current line number.
-        /// </summary>
-        /// <param name="message">The message that describes the error.</param>
-        /// <param name="actualValue">The actual value that caused the exception.</param>
-        /// <param name="currentLine">The current line that caused the exception.</param>
-        public ParserException(
-            string message, 
-            object actualValue = null, 
-            int? currentLine = null) : base(message)
+        public ParserException()
         {
-            _actualValue = actualValue;
-            _currentLine = currentLine;
+
         }
 
         /// <summary>
-        /// Gets a message that describes the current exception.
-        /// </summary>
-        public override string Message => FormatErrorMessage(base.Message, _actualValue, _currentLine);
-
-        /// <summary>
-        /// Formats an error message.
+        /// Initializes a new instance of the <see cref="ParserException" /> class with the a specified error message.
         /// </summary>
         /// <param name="message">The message that describes the error.</param>
-        /// <param name="actualValue">The actual value that caused the error.</param>
-        /// <param name="lineNumber">The line number that caused the error.</param>
-        /// <param name="envFileName">The name of the .env file that caused the error.</param>
-        /// <returns>A formatted error message.</returns>
-        internal static string FormatErrorMessage(string message, object actualValue = null, int? lineNumber = null, string envFileName = null)
+        public ParserException(string message) : base(message)
         {
-            if(actualValue != null && lineNumber != null && envFileName != null)
-                return $"{message} (Actual Value: {actualValue}, Line: {lineNumber}, FileName: {envFileName})";
-
-            if (actualValue != null && lineNumber != null)
-                return $"{message} (Actual Value: {actualValue}, Line: {lineNumber})";
-
-            if (envFileName != null && lineNumber != null)
-                return $"{message} (Line: {lineNumber}, FileName: {envFileName})";
-
-            if (lineNumber != null)
-                return $"{message} (Line: {lineNumber})";
-
-            if (actualValue != null)
-                return $"{message} (Actual Value: {actualValue})";
-
-            if (envFileName != null)
-                return $"{message} (FileName: {envFileName})";
-
-            return message;
         }
     }
 }
