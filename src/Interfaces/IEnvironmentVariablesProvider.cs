f51fc5b2ba8b15939f248bd6afd666d70d682b81
MrDave1999 <daveseva2010@hotmail.es>
2022-03-06 18:34:55 -0500

    The XML documentation of a property has been updated

--- src/Interfaces/IEnvironmentVariablesProvider.cs
+++ src/Interfaces/IEnvironmentVariablesProvider.cs
@@ -14,6 +14,7 @@ namespace DotEnv.Core
         /// </summary>
         /// <param name="variable">The variable to get or set.</param>
         /// <exception cref="ArgumentNullException"><c>variable</c> is <c>null</c>.</exception>
+        /// <remarks>The property gets a <c>null</c> value in case the <c>variable</c> is not found in the provider.</remarks>
         string this[string variable] { get; set; }
     }
 }
