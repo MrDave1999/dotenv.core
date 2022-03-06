edea9b289b773fb75711da8a8e548555704e5d8d
MrDave1999 <daveseva2010@hotmail.es>
2022-03-06 16:45:07 -0500

    Added the 'EnvironmentVariablesProviderExtensions' class (issue #42)

---
+++ src/Extensions/EnvironmentVariablesProviderExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotEnv.Core
+{
+    /// <summary>
+    /// Extension methods for adding <see cref="IEnvironmentVariablesProvider"/>.
+    /// </summary>
+    public static class EnvironmentVariablesProviderExtensions
+    {
+        /// <summary>
+        /// Creates an instance that implements the <see cref="IEnvValidator" /> interface.
+        /// </summary>
+        /// <param name="envVars">The environment variables provider.</param>
+        /// <returns>An instance that implements the <see cref="IEnvValidator" /> interface.</returns>
+        public static IEnvValidator CreateValidator(this IEnvironmentVariablesProvider envVars)
+            => new EnvValidator(envVars);
+
+        /// <summary>
+        /// Creates an instance that implements the <see cref="IEnvReader" /> interface.
+        /// </summary>
+        /// <param name="envVars">The environment variables provider.</param>
+        /// <returns>An instance that implements the <see cref="IEnvReader" /> interface.</returns>
+        public static IEnvReader CreateReader(this IEnvironmentVariablesProvider envVars)
+            => new EnvReader(envVars);
+
+        /// <summary>
+        /// Converts the environment variables provider to a dictionary.
+        /// </summary>
+        /// <param name="envVars">The environment variables provider.</param>
+        /// <returns>A dictionary with the environment variables.</returns>
+        public static IDictionary<string, string> ToDictionary(this IEnvironmentVariablesProvider envVars)
+        {
+            var keyValuePairs = new Dictionary<string, string>();
+            foreach (var keyValuePair in envVars)
+                keyValuePairs[keyValuePair.Key] = keyValuePair.Value;
+            return keyValuePairs;
+        }
+    }
+}
