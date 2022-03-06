97636343db7118d69a7233c8ad17ec91a79ed53e
MrDave1999 <daveseva2010@hotmail.es>
2022-03-06 13:57:29 -0500

    Added the providers that implement the 'IEnvironmentVariablesProvider' interface (issue #20)

---
+++ src/VariablesProvider/DefaultEnvironmentProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotEnv.Core
+{
+    /// <summary>
+    ///  Represents the variables provider using the environment of the current process.
+    /// </summary>
+    internal class DefaultEnvironmentProvider : IEnvironmentVariablesProvider
+    {
+        /// <inheritdoc />
+        public string this[string variable] 
+        {
+            get => Environment.GetEnvironmentVariable(variable);
+            set => Environment.SetEnvironmentVariable(variable, value);
+        }
+
+        public IEnumerator<KeyValuePair<string, string>> GetEnumerator()
+        {
+            foreach (DictionaryEntry de in Environment.GetEnvironmentVariables())
+                yield return new KeyValuePair<string, string>((string)de.Key, (string)de.Value);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => this.GetEnumerator();
+    }
+}
