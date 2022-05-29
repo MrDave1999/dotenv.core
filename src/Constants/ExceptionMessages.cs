7121bc9bedf564322bbc02a6b2ad7271f5440c37
MrDave1999 <daveseva2010@hotmail.es>
2022-05-29 14:21:22 -0500

    Added additional position marker to a constant

--- src/Constants/ExceptionMessages.cs
+++ src/Constants/ExceptionMessages.cs
@@ -21,7 +21,7 @@ namespace DotEnv.Core
         public const string RequiredKeysNotPresentMessage = "'{0}' is a key required by the application.";
         public const string LengthOfParamsListIsZeroMessage = "The length of the params list is zero.";
         public const string EncodingNotFoundMessage = "'{0}' is not a supported encoding name. For information on defining a custom encoding, see the documentation for the Encoding.RegisterProvider method.";
-        public const string PropertyDoesNotMatchConfigKeyMessage = "The '{0}' property does not match any configuration key.";
+        public const string PropertyDoesNotMatchConfigKeyMessage = "The '{0}.{1}' property does not match any configuration key.";
         public const string KeyAssignedToPropertyIsNotSetMessage = "Could not set the value in the '{0}.{1}' property because the '{2}' key is not set.";
         public const string FailedConvertConfigurationValueMessage = "Failed to convert configuration value of '{0}' to type '{1}'. '{2}' is not a valid value for {3}.";
     }
