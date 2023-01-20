d5f453dce6767a1154a5f821256bd5209a2a938e
MrDave1999 <daveseva2010@hotmail.es>
2023-01-20 18:08:34 -0500

    Added 'EnvBinder.HelperMethods' file

---
+++ src/Binder/EnvBinder.HelperMethods.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+
+namespace DotEnv.Core;
+
+// This class defines the helper private methods.
+public partial class EnvBinder
+{
+    /// <summary>
+    /// Checks whether the property is read-only or write-only.
+    /// </summary>
+    /// <returns><c>true</c> if the property is read-only or write-only, or <c>false</c> if the property is read-write.</returns>
+    private bool IsReadOnlyOrWriteOnly(PropertyInfo property)
+        => !property.CanRead || !property.CanWrite;
+}
