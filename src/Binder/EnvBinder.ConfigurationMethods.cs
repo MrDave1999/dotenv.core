28f0783787c38cdf352fd9ef419fc4ce2df551ea
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2023-01-20 18:13:05 -0500

    Converted 'EnvBinder' class to partial class (#120)
    * Added 'EnvBinder.HelperMethods' file
    
    * Added 'EnvBinder.ConfigurationMethods' file

---
+++ src/Binder/EnvBinder.ConfigurationMethods.cs
@@ -0,0 +1,19 @@
+﻿namespace DotEnv.Core;
+
+// This class defines the configuration methods that will be used to change the behavior of the binder.
+public partial class EnvBinder
+{
+    /// <inheritdoc />
+    public IEnvBinder IgnoreException()
+    {
+        _configuration.ThrowException = false;
+        return this;
+    }
+
+    /// <inheritdoc />
+    public IEnvBinder AllowBindNonPublicProperties()
+    {
+        _configuration.BindNonPublicProperties = true;
+        return this;
+    }
+}
