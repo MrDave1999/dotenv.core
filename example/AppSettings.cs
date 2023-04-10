213c00c0b6226b53b0d79761f17b8d4d3a447404
MrDave1999 <daveseva2010@hotmail.es>
2023-04-10 09:46:29 -0500

    Added a simpler and more understandable example

--- example/AppSettings.cs
+++ example/AppSettings.cs
@@ -1,5 +1,6 @@
 public class AppSettings
 {
-    public string? MysqlHost { get; set; }
-    public string? MysqlDb { get; set; }
+    public const string DbHost           = "DB_HOST";
+    public const string DbUserName       = "DB_USERNAME";
+    public const string ConnectionString = "DB_CONNECTION_STRING";
 }
