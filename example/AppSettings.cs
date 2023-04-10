49608500579ae1e01889be4cdac7cf4e46b51ad7
Dave Roman <43916038+MrDave1999@users.noreply.github.com>
2023-04-10 09:55:28 -0500

    Added a more understandable example (#125)
    * Removed some .env files
    
    * Removed GlobalUsings file
    
    * Added `AppSettings` class in `Program.cs`

--- example/AppSettings.cs
+++
@@ -1,5 +0,0 @@
-public class AppSettings
-{
-    public string? MysqlHost { get; set; }
-    public string? MysqlDb { get; set; }
-}
--- example/Program.cs
+++ example/Program.cs
@@ -1,115 +1,20 @@
+using DotEnv.Core;
+
+var envVars = new EnvLoader().Load();
+
+var cs = envVars[AppSettings.ConnectionString];
+Console.WriteLine("Connection String=" + cs);
+
 var reader = new EnvReader();
+var userName = reader[AppSettings.DbUserName];
+Console.WriteLine("UserName=" + userName);
 
-Console.WriteLine("---- EXAMPLE (1):");
-new EnvLoader()
-    .EnableFileNotFoundException()
-    .Load();
-Console.WriteLine($"MYSQL_HOST={reader["MYSQL_HOST"]}");
-Console.WriteLine($"MYSQL_DB={reader["MYSQL_DB"]}");
-Console.WriteLine($"PRIVATE_KEY={reader["PRIVATE_KEY"]}");
-Console.WriteLine("\n\n\n");
+var dbHost = EnvReader.Instance[AppSettings.DbHost];
+Console.WriteLine("Host=" + dbHost);
 
-Console.WriteLine("---- SPECIAL EXAMPLE:");
-var settings = new EnvBinder().Bind<AppSettings>();
-Console.WriteLine($"MYSQL_HOST={settings.MysqlHost}");
-Console.WriteLine($"MYSQL_DB={settings.MysqlDb}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (2):");
-new EnvLoader()
-    .SetBasePath("files")
-    .Load();
-Console.WriteLine($"MARIADB_HOST={reader["MARIADB_HOST"]}");
-Console.WriteLine($"MARIADB_DB={reader["MARIADB_DB"]}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (3):");
-new EnvLoader()
-    .AddEnvFile("./files/.env.example")
-    .Load();
-Console.WriteLine($"MONGODB_HOST={reader["MONGODB_HOST"]}");
-Console.WriteLine($"MONGODB_DB={reader["MONGODB_DB"]}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (4):");
-new EnvLoader()
-    .AddEnvFiles("files/sqlite")
-    .EnableFileNotFoundException()
-    .Load();
-Console.WriteLine($"SQLITE_HOST={reader["SQLITE_HOST"]}");
-Console.WriteLine($"SQLITE_DB={reader["SQLITE_DB"]}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (5):");
-new EnvLoader()
-    .SetDefaultEnvFileName(".env.local")
-    .SetBasePath("./files/pgsql")
-    .Load();
-Console.WriteLine($"PGSQL_HOST={reader["PGSQL_HOST"]}");
-Console.WriteLine($"PGSQL_DB={reader["PGSQL_DB"]}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (6):");
-EnvValidationResult resultExample6;
-new EnvLoader()
-    .IgnoreParserException()
-    .SetDefaultEnvFileName("./files/.env.local")
-    .Load(out resultExample6);
-
-Console.WriteLine($"API_KEY LENGTH={reader["API_KEY"].Length}");
-Console.WriteLine(resultExample6.ErrorMessages);
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (7):");
-var keyValuePairs = new EnvLoader()
-                        .AvoidModifyEnvironment()
-                        .AddEnvFiles(
-                            "./", 
-                            "./files/", 
-                            "files/sqlite", 
-                            "./files/pgsql/.env.local"
-                          )
-                        .Load();
-Console.WriteLine("-> Dictionary<string, string>:");
-foreach (var keyValuePair in keyValuePairs.ToDictionary())
-    Console.WriteLine($"{keyValuePair.Key}, {keyValuePair.Value}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (8):");
-EnvValidationResult resultExample8;
-new EnvLoader()
-    .SetBasePath("./files/environment")
-    .AllowConcatDuplicateKeys()
-    .LoadEnv(out resultExample8);
-if (resultExample8.HasError())
-    Console.WriteLine(resultExample8.ErrorMessages);
-else
-    Console.WriteLine($"CONNECTION_STRING_MSSQL={reader["CONNECTION_STRING_MSSQL"]}");
-Console.WriteLine("\n\n\n");
-
-Console.WriteLine("---- EXAMPLE (9):");
-EnvValidationResult resultExample9;
-var envVars = new EnvLoader()
-                  .AllowConcatDuplicateKeys()
-                  .SetBasePath("./files/environment")
-                  .AvoidModifyEnvironment()
-                  .LoadEnv(out resultExample9);
-
-if (resultExample9.HasError())
-    Console.WriteLine(resultExample9.ErrorMessages);
-else
+public class AppSettings
 {
-    foreach (var keyValuePair in envVars)
-        Console.WriteLine($"{keyValuePair.Key}, {keyValuePair.Value}");
+    public const string DbHost           = "DB_HOST";
+    public const string DbUserName       = "DB_USERNAME";
+    public const string ConnectionString = "DB_CONNECTION_STRING";
 }
-Console.WriteLine("\n\n\n");
-
-Console.Write("---- EXAMPLE (10):");
-EnvValidationResult resultExample10;
-new EnvValidator()
-    .SetRequiredKeys("SERVICE_ID", "TOKEN_ID")
-    .IgnoreException()
-    .Validate(out resultExample10);
-
-Console.Write(resultExample10.ErrorMessages);
-Console.WriteLine("\n\n\n");
