7eb11d82bf0433b3ca4028058aa235797143c644
MrDave1999 <daveseva2010@hotmail.es>
2023-04-27 11:37:23 -0500

    Added a usage example for the generic method.

--- example/Program.cs
+++ example/Program.cs
@@ -15,9 +15,13 @@ Console.WriteLine("Host=" + dbHost);
 var dbPassword = AppSettings.DbPassword.GetEnv();
 Console.WriteLine("Password=" + dbPassword);
 
+int dbPort = AppSettings.DbPort.GetEnv<int>();
+Console.WriteLine("Port=" + dbPort);
+
 public class AppSettings
 {
     public const string DbHost           = "DB_HOST";
+    public const string DbPort           = "DB_PORT";
     public const string DbUserName       = "DB_USERNAME";
     public const string DbPassword       = "DB_PASSWORD";
     public const string ConnectionString = "DB_CONNECTION_STRING";
