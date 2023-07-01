6653bdf22a53de88dafe81b2f56486d1c9f3a656
MrDave1999 <daveseva2010@hotmail.es>
2023-07-01 14:17:42 -0500

    Added the `DotEnv.Example.ConfigurationApi` example

---
+++ samples/DotEnv.Example.ConfigurationApi/Program.cs
@@ -0,0 +1,32 @@
+using DotEnv.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+// Loads the .env file.
+new EnvLoader().Load();
+// Adds environment variables in the configuration collection.
+builder.Configuration.AddEnvironmentVariables();
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
