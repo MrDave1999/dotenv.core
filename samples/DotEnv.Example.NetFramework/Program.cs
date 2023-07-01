4005146ab441ef2bf3ccad3a4edd294f11517fad
MrDave1999 <daveseva2010@hotmail.es>
2023-07-01 14:47:32 -0500

    Added the `DotEnv.Example.NetFramework` example

---
+++ samples/DotEnv.Example.NetFramework/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DotEnv.Core;
+
+namespace DotEnv.Example.NetFramework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var envVars = new EnvLoader().Load();
+            Console.WriteLine(envVars["APP_BASE_URL"]);
+
+            var reader = new EnvReader();
+            Console.WriteLine(reader["APP_BASE_URL"]);
+
+            Console.ReadLine();
+        }
+    }
+}
