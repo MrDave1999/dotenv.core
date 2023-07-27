05aacd7f1e4b1a4ea3067c2b169b8b2365599e36
MrDave1999 <daveseva2010@hotmail.es>
2023-07-27 18:27:04 -0500

    Added a sample project about the using parser

---
+++ samples/DotEnv.Example.Parser/Program.cs
@@ -0,0 +1,12 @@
+﻿using DotEnv.Core;
+
+var source = 
+"""
+ DB_HOST=localhost
+ DB_USERNAME=mrdave1999
+ DB_PASSWORD=123456789$
+""";
+var envVars = new EnvParser().Parse(source);
+Console.WriteLine("DB_HOST="     + envVars["DB_HOST"]);
+Console.WriteLine("DB_USERNAME=" + envVars["DB_USERNAME"]);
+Console.WriteLine("DB_PASSWORD=" + envVars["DB_PASSWORD"]);
