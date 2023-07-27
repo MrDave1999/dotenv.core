2b5a92283b6600dd797127e19635f781d3e27079
MrDave1999 <daveseva2010@hotmail.es>
2023-07-27 13:50:36 -0500

    Added a sample project about using required keys

---
+++ samples/DotEnv.Example.RequiredKeys/Program.cs
@@ -0,0 +1,21 @@
+﻿using DotEnv.Core;
+
+var requiredKeys = new[]
+{
+    "DB_PORT",
+    "DB_USERNAME",
+    "DB_PASSWORD",
+    "DB_DATABASE"
+};
+var envVars = new EnvLoader().Load();
+Console.WriteLine("DB_HOST=" + envVars["DB_HOST"]);
+
+// Checks if the required keys are present in the application.
+// In this case, the application will fail because the following keys are not present in the .env file:
+// DB_PORT
+// DB_USERNAME
+// DB_PASSWORD
+// DB_DATABASE
+new EnvValidator()
+    .SetRequiredKeys(requiredKeys)
+    .Validate();
