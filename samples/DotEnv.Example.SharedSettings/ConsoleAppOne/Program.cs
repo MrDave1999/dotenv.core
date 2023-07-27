2ad425c7d1899c39b1e0ee77d4918d174d335937
MrDave1999 <daveseva2010@hotmail.es>
2023-07-27 12:49:36 -0500

    Added a sample project on shared settings (.env)

---
+++ samples/DotEnv.Example.SharedSettings/ConsoleAppOne/Program.cs
@@ -0,0 +1,20 @@
+﻿using DotEnv.Core;
+
+// Loads the config.env file.
+new EnvLoader()
+    .EnableFileNotFoundException()
+    .SetBasePath("Shared")
+    .AddEnvFile("config.env")
+    .Load();
+
+var reader = new EnvReader();
+Console.WriteLine("APP_BASE_URL=" + reader["APP_BASE_URL"]);
+
+// Loads the .env.dev.local and .env.local files.
+new EnvLoader()
+    .SetBasePath("Shared")
+    .EnableFileNotFoundException()
+    .LoadEnv();
+
+Console.WriteLine("DB_USERNAME=" + reader["DB_USERNAME"]);
+Console.WriteLine("DB_PASSWORD=" + reader["DB_PASSWORD"]);
