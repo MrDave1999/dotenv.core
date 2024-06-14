dd816ff23bd02a408dcd2c64c874e276afd53bd6
MrDave1999 <daveseva2010@hotmail.es>
2024-06-14 17:25:12 -0500

    cleanup: Remove unused "using"

--- src/Loader/EnvLoader.HelperMethods.cs
+++ src/Loader/EnvLoader.HelperMethods.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using static DotEnv.Core.ExceptionMessages;
-using static DotEnv.Core.FormattingMessage;
 
 namespace DotEnv.Core;
 
