56991acdc7ec9bc1f8fc9eb49cef205dce66b26f
MrDave1999 <daveseva2010@hotmail.es>
2024-06-23 11:56:58 -0500

    feat: Add support for Microsoft.Extensions.Configuration

---
+++ plugins/Microsoft.Extensions.Config/src/EnvConfigurationExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DotEnv.Core;
+
+namespace Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Extension methods for adding <see cref="EnvConfigurationProvider"/>.
+/// </summary>
+public static class EnvConfigurationExtensions
+{
+    /// <summary>
+    /// Adds the ENV configuration provider at <paramref name="path"/> to <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+    /// <param name="path">The path to the file.</param>
+    /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="builder"/>, or <paramref name="path"/> is <c>null</c>.
+    /// </exception>
+    public static IConfigurationBuilder AddEnvFile(this IConfigurationBuilder builder, string path)
+        => builder.AddEnvFile(path, optional: false);
+
+    /// <summary>
+    /// Adds a ENV configuration source to <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+    /// <param name="path">The path to the file.</param>
+    /// <param name="optional">Whether the file is optional.</param>
+    /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="builder"/>, or <paramref name="path"/> is <c>null</c>.
+    /// </exception>
+    public static IConfigurationBuilder AddEnvFile(
+        this IConfigurationBuilder builder,
+        string path,
+        bool optional)
+    {
+        ThrowHelper.ThrowIfNull(builder, nameof(builder));
+        ThrowHelper.ThrowIfNullOrWhiteSpace(path, nameof(path));
+        return builder.AddEnvFile(source =>
+        {
+            source.Path = path;
+            source.Optional = optional;
+        });
+    }
+
+    /// <summary>
+    /// Adds a ENV configuration source to <paramref name="builder"/>.
+    /// </summary>
+    /// <param name="builder">The <see cref="IConfigurationBuilder"/> to add to.</param>
+    /// <param name="configureSource">Configures the source.</param>
+    /// <returns>The <see cref="IConfigurationBuilder"/>.</returns>
+    public static IConfigurationBuilder AddEnvFile(
+        this IConfigurationBuilder builder,
+        Action<EnvConfigurationSource> configureSource)
+        => builder.Add(configureSource);
+}
---
+++ plugins/Microsoft.Extensions.Config/src/EnvConfigurationSource.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Extensions.Configuration;
+
+/// <summary>
+/// Represents an ENV file as an <see cref="IConfigurationSource"/>.
+/// </summary>
+/// <examples>
+/// KEY1=VALUE1
+/// # comment
+/// </examples>
+public class EnvConfigurationSource : IConfigurationSource
+{
+    /// <summary>
+    /// The path to the file.
+    /// </summary>
+    public string Path { get; set; }
+
+    /// <summary>
+    /// Determines if loading the file is optional.
+    /// </summary>
+    public bool Optional { get; set; }
+
+    /// <summary>
+    /// Builds the <see cref="EnvConfigurationProvider"/> for this source.
+    /// </summary>
+    /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+    /// <returns>An <see cref="EnvConfigurationProvider"/></returns>
+    public IConfigurationProvider Build(IConfigurationBuilder builder)
+        => new EnvConfigurationProvider(this);
+}
